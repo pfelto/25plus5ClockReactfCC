729bf301408f7a4cb6bc433dee01db58c956fa23
pfelto <paul.r.felton@gmail.com>
2022-03-16 09:05:16 -0400

    Passed all the tests and just need to style before submitting to fCC

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 
-const sound = { src: "https://www.myinstants.com/media/sounds/beep.mp3" };
-
 const STATUS = {
   IDLE: "IDLE",
   ACTIVE: "ACTIVE",
@@ -12,12 +10,20 @@ function LengthComponent({ status, title, length, handleClick }) {
 
   return (
     <div>
-      <h2>{title}</h2>
-      <button onClick={() => handleClick(-1, title)} disabled={isACTIVE}>
+      <h2 id={`${title.toLowerCase()}-label`}>{title} Length</h2>
+      <button
+        id={`${title.toLowerCase()}-decrement`}
+        onClick={() => handleClick(-1, title)}
+        disabled={isACTIVE}
+      >
         decrement
       </button>
-      {length}
-      <button onClick={() => handleClick(1, title)} disabled={isACTIVE}>
+      <p id={`${title.toLowerCase()}-length`}>{length}</p>
+      <button
+        id={`${title.toLowerCase()}-increment`}
+        onClick={() => handleClick(1, title)}
+        disabled={isACTIVE}
+      >
         increment
       </button>
     </div>
@@ -25,10 +31,20 @@ function LengthComponent({ status, title, length, handleClick }) {
 }
 
 function SessionClock({ title, length }) {
+  let minutes = Math.floor(length / 60);
+  let seconds = length - minutes * 60;
+  let formattedMin = ("0" + minutes).slice(-2);
+  let formattedSeconds = ("0" + seconds).slice(-2);
   return (
     <div>
-      <h2>{title}</h2>
-      {length}
+      <h2 id="timer-label">{title}</h2>
+      <p id="time-left">
+        {formattedMin}:{formattedSeconds}
+      </p>
+      <audio
+        id="beep"
+        src="https://www.myinstants.com/media/sounds/alarm.mp3"
+      ></audio>
     </div>
   );
 }
@@ -36,18 +52,22 @@ function SessionClock({ title, length }) {
 function App() {
   const [sessionLength, setSessionLength] = useState(25);
   const [breakLength, setBreakLength] = useState(5);
-  const [timer, setTimer] = useState({ id: "Session", length: 25 });
+  const [timer, setTimer] = useState({ id: "Session", length: 1500 });
   const [status, setStatus] = useState(STATUS.IDLE);
 
   useEffect(() => {
-    if (timer.length === -1) {
+    if (timer.length === 0) {
+      //play audio
+      const sound = document.getElementById("beep");
+      sound.currentTime = 0;
+      sound.play();
       if (timer.id === "Session") {
-        setTimer({ id: "Break", length: breakLength });
+        setTimer({ id: "Break", length: breakLength * 60 });
       } else {
-        setTimer({ id: "Session", length: sessionLength });
+        setTimer({ id: "Session", length: sessionLength * 60 });
       }
     }
-    if (timer.length > -1 && status !== STATUS.IDLE) {
+    if (timer.length > 0 && status !== STATUS.IDLE) {
       const timerId = setTimeout(() => {
         setTimer({ ...timer, length: timer.length - 1 });
       }, 1000);
@@ -56,6 +76,7 @@ function App() {
   }, [timer, status, breakLength, sessionLength]);
 
   function handleClick(value, title) {
+    let newValue = value * 60;
     if (title === "Session") {
       if (
         (sessionLength === 60 && value === 1) ||
@@ -64,7 +85,9 @@ function App() {
         return;
       }
       setSessionLength((sessionLength) => sessionLength + value);
-      setTimer({ ...timer, length: sessionLength + value });
+      if (timer.id === "Session") {
+        setTimer({ ...timer, length: sessionLength * 60 + newValue });
+      }
     } else {
       if (
         (breakLength === 60 && value === 1) ||
@@ -73,7 +96,9 @@ function App() {
         return;
       }
       setBreakLength((breakLength) => breakLength + value);
-      setTimer({ ...timer, length: breakLength + value });
+      if (timer.id === "Break") {
+        setTimer({ ...timer, length: breakLength * 60 + newValue });
+      }
     }
   }
 
@@ -98,6 +123,7 @@ function App() {
         <SessionClock title={timer.id} length={timer.length} />
       )}
       <button
+        id="start_stop"
         onClick={() => {
           if (status === STATUS.IDLE) setStatus(STATUS.ACTIVE);
           else setStatus(STATUS.IDLE);
@@ -106,10 +132,17 @@ function App() {
         {status === STATUS.IDLE ? "play" : "stop"}
       </button>
       <button
+        id="reset"
         onClick={() => {
+          //if audio is playing. Stop it and set currentTime to 0
+          const sound = document.getElementById("beep");
+          if (!sound.paused) {
+            sound.pause();
+            sound.currentTime = 0;
+          }
           setSessionLength(25);
           setBreakLength(5);
-          setTimer({ id: "Session", length: 25 });
+          setTimer({ id: "Session", length: 25 * 60 });
           setStatus(STATUS.IDLE);
         }}
       >
